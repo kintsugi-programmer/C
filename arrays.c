dfb334e4b68c0afb6fdc57b9647f6ca776fd2ecc
balibhai1010 <siddhant22496@iiitd.ac.in>
2024-01-05 02:05:32 +0530

    update array initialization

--- arrays.c
+++ arrays.c
@@ -4,6 +4,15 @@
 int main()
 {
    // array = a data structure that can store many values of the same data type.
+   //          each value is stored in a numbered "slot".
+   //          arrays have a fixed size.
+   //          arrays are created in the stack.
+   //          arrays are created using [].
+   //          arrays are 0-indexed.
+   //          arrays can be initialized using {}.
+   //          arrays can be initialized with a size or without a size.
+   //          arrays can be initialized with a size and values or without a size and values.
+
 
    //double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0};
    double prices[5];
--- nested_loop.c
+++ nested_loop.c
@@ -1,4 +1,4 @@
-
+//error in this code?
 #include <stdio.h>
 
 int main()
